d03cbf652ec1c0543b4ca3b074ec04b73e156f98
mdelaneycastle <154420982+mdelaneycastle@users.noreply.github.com>
2025-06-16 22:08:40 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,15 @@
 // firebase-init.js
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDJvHNZB_pzhytD7yLa69auStrZBk2SEHk",
-      authDomain: "castle-comms.firebaseapp.com",
-      databaseURL: "https://castle-comms-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "castle-comms",
-      storageBucket: "castle-comms.appspot.com",
-      messagingSenderId: "959264765744",
-      appId: "1:959264765744:web:7aee345a37673f720cfaf5",
-      measurementId: "G-NLS757VMJ8"
+  authDomain: "castle-comms.firebaseapp.com",
+  databaseURL: "https://castle-comms-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "castle-comms",
+  storageBucket: "castle-comms.appspot.com",
+  messagingSenderId: "959264765744",
+  appId: "1:959264765744:web:7aee345a37673f720cfaf5",
+  measurementId: "G-NLS757VMJ8"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.database();  // <-- Make db globally accessible
