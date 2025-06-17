f6b0db79e96132835c4b42ec6a0f17172171689b
mdelaneycastle <154420982+mdelaneycastle@users.noreply.github.com>
2025-06-17 15:24:15 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   authDomain: "castle-comms.firebaseapp.com",
   databaseURL: "https://castle-comms-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "castle-comms",
-  storageBucket: "castle-comms.appspot.com",
+  storageBucket: "castle-comms.firebasestorage.app",
   messagingSenderId: "959264765744",
   appId: "1:959264765744:web:7aee345a37673f720cfaf5",
   measurementId: "G-NLS757VMJ8"
